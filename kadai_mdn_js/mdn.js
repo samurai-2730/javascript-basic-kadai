372ab582dec4562c6b3cbf21bce035c04536be31
samurai-2730 <kazarisa46@gmail.com>
2025-10-17 09:43:16 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,7 @@
 var date = new Date();
-console.log(date.getFullYear() + '年', date.getMonth() + 1 + '月', date.getDate() + '日');
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+
+console.log(year + '年' + month + '月' + day + '日');
+//console.log(date.getFullYear() + '年', date.getMonth() + 1 + '月'+ date.getDate() + '日');
